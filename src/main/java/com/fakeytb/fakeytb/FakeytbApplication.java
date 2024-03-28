eeddcade22ed9254059a34191cf977d5a787a6a7
Nwlcnf <153742803+Nwlcnf@users.noreply.github.com>
2024-03-28 10:25:24 +0100

    Initial commit

---
+++ src/main/java/com/fakeytb/fakeytb/FakeytbApplication.java
@@ -0,0 +1,13 @@
+package com.fakeytb.fakeytb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeytbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeytbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fakeytb/fakeytb/FakeytbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fakeytb.fakeytb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeytbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
